fefe88e9b7d2367de63b14964e66f947e1d7f7ba
yuu-eguci <yuu.eguci@gmail.com>
2020-03-01 17:35:15 +0900

    Create ASP.NET MVC project

---
+++ i18nApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace i18nApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ i18nApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="i18nApp.MvcApplication" Language="C#" %>
