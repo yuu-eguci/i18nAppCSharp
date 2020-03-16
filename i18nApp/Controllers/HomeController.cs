53cffc3f2c6875ce3de622b7d432f1c87c4e1e18
yuu-eguci <yuu.eguci@gmail.com>
2020-03-16 18:40:04 +0900

    #17 :recycle: No need of abstract class this time

--- i18nApp/Controllers/HomeController.cs
+++ i18nApp/Controllers/HomeController.cs
@@ -9,7 +9,15 @@ using i18nApp.Helpers;
 
 namespace i18nApp.Controllers
 {
-    public class HomeController : LoggingController
+    /// <summary>
+    /// Run log4net in the Order:1 OnActionExecuting in all derived classes.
+    /// Set Order:1 to run this after I18nAttribute.
+    /// Only if using I18nAttribute for all action methods, just writing like below will work.
+    ///     [I18nAttribute]
+    ///     [AccessLoggingAttribute]
+    /// </summary>
+    [AccessLoggingAttribute(Order = 1)]
+    public class HomeController : Controller
     {
         public ActionResult Index()
         {
