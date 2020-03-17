6cbc69a32134a295b3e1f9ea0e918482fbaa1900
yuu-eguci <yuu.eguci@gmail.com>
2020-03-17 18:31:24 +0900

    :pencil: Add some comments

--- i18nApp/Helpers/AccessLoggingAttribute.cs
+++ i18nApp/Helpers/AccessLoggingAttribute.cs
@@ -21,6 +21,13 @@ namespace i18nApp.Helpers
                 // Name this logger with this class name.
                 controllerType);
             logger.Info($"Access: {controllerType}.{actionMethodName}, Culture: {Thread.CurrentThread.CurrentCulture.Name}");
+
+            // NOTE:
+            // Controller name - filterContext.ActionDescriptor.ControllerDescriptor.ControllerType.FullName
+            // Action name: filterContext.ActionDescriptor.ActionName
+            // Caller controller: filterContext.Controller
+            // Use caller controller's member: (FooController)filterContext.Controller; fooController.Bar;
+            // Action method http method: filterContext.HttpContext.Request.HttpMethod
         }
     }
 }
